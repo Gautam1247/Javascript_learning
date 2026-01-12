71a518ea96213d436ed8b3f2d5c49207b677ec72
Gautam1247 <code.grs12@gmail.com>
2026-01-12 10:13:42 +0530

    final changes

--- oots/prototype.js
+++ oots/prototype.js
@@ -1,27 +1,27 @@
-const arr=["spidey","hulk","ironman"]
-Array.prototype.printname=function(){
-    console.log("my name is gautam raj singh");
-}
-arr.printname()
+// const arr=["spidey","hulk","ironman"]
+// Array.prototype.printname=function(){
+//     console.log("my name is gautam raj singh");
+// }
+// arr.printname()
 
 
-const str="Gautam"
-String.prototype.add=function(){
-    console.log(this.concat("raj"));
-    console.log(this);
+// const str="Gautam"
+// String.prototype.add=function(){
+//     console.log(this.concat("raj"));
+//     console.log(this);
     
-}
-str.add()
-const school={
-    name:"gautam",
-    class:"10D"
-}
-const college={
-    name:"gouri",
-    classs:"11A",
-    __proto__:school
-}
-console.log(college.class);
+// }
+// str.add()
+// const school={
+//     name:"gautam",
+//     class:"10D"
+// }
+// const college={
+//     name:"gouri",
+//     classs:"11A",
+//     __proto__:school
+// }
+//console.log(college.class);
 const school2={
     name:"gautam",
     class:"10D"
